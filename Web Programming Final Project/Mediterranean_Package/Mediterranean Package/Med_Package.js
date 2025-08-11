27d1be2e018d6b25fbe4eaa3e472adacaa1e3ec6
Bralyn Loach-Perry <bralynlp@gmail.com>
2025-08-11 17:38:03 -0400

    Add files via upload

---
+++ Web Programming Final Project/Mediterranean_Package/Mediterranean Package/Med_Package.js
@@ -0,0 +1,18 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_container h1", scrollRevealOption)
+
+ScrollReveal().reveal(".header_container h3",{
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".Greece", {...scrollRevealOption, interval: 500,});
+
+ScrollReveal().reveal(".France", {...scrollRevealOption, interval: 500,});
+
+ScrollReveal().reveal(".Italy", {...scrollRevealOption, interval: 500,});
